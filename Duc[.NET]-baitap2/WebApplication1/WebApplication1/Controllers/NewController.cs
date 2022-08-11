5dad6fa5089ba148ab5fe96d65b88e8fc7025e01
NguyenDuc96a <narutonvd@gmail.com>
2022-08-11 11:42:21 +0700

    click me

---
+++ Duc[.NET]-baitap2/WebApplication1/WebApplication1/Controllers/NewController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class NewController : Controller
+    {
+        /*
+        // GET: NewController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: NewController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NewController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NewController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NewController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        */
+        public IActionResult Index()
+
+        {
+            var articles = new List<Article>
+            {
+               new Article{Id=1,Title="Title01",Content="This is content 1",Author="Ducdaica"},
+                new Article{Id=2,Title="Title02",Content="This is content 2",Author="Ducdaica"},
+                 new Article{Id=3,Title="Title03",Content="This is content 3",Author="Ducdaica"},
+            new Article { Id = 4, Title = "Title04", Content = "This is content 4", Author = "Ducdaica" },
+        };
+            // cach 1: using ViewBag
+            // ViewBag.Article = articles;
+
+            // cach 2: using ViewData
+          //  ViewData["Articles"] = articles;
+            // cach 3: using Model
+            return View(articles);
+        }
+      //  public String StringOut(int id, string firstname, string lastname)
+        public String StringOut(int id,Empoyee empoyee)
+        {
+            return ($"Say hello from DKM : My number  is {id} my full name is {empoyee.FirstName}{empoyee.LastName}");
+        }
+
+        public IActionResult StringOut2(int id, Empoyee empoyee)
+        {
+            var obj = new { Id = id, Empoyee = empoyee };
+            return Json(obj);
+        }
+    }
+    public class Empoyee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ Duc[.NET]-baitap2/WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var connectionString = Configuration.GetConnectionString("ASPcore5Database"); // dang ki bien moi truong cho connectionString
+            services.AddDbContext<ASPcore5_DBContext>(options => options.UseSqlServer(connectionString)); // add services vao db Context
+
+           // services.Configure<Services.SmtpConfigurations>(Configuration);
+          //  services.AddSingleton<Services.SmtpHandling, Services.SmtpHandling>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+
+            {
+                endpoints.MapControllerRoute(
+                   name: "New",
+                   pattern: "Duc",
+                   new {controller = "New",action = "Index" });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
